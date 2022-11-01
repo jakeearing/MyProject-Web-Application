85bcf19f6c419105eba53858316de773bbf60c3a
Jake Earing <jakeearing7@gmail.com>
2022-11-01 14:43:19 -0400

    Added routes for /projects and /account
    Added routes for /projects and /account, these will render the projects.html and account.html pages respectively.

--- app.py
+++ app.py
@@ -10,3 +10,11 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/projects')
+def index():
+    return render_template('projects.html')
+
+@app.route('/account')
+def index():
+    return render_template('account.html')
