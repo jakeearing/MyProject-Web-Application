70f21223ac33ff2fa2b8d28eed0cfc59bcf433c8
Jake Earing <jakeearing7@gmail.com>
2022-11-01 14:12:45 -0400

    App.py index route added

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import os
 from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect, url_for
 
 app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
