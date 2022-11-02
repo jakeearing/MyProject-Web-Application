bf56de7f6e2e7ec01c5517232c6c97784d683388
Jake Earing <jakeearing7@gmail.com>
2022-11-01 23:11:58 -0400

    database and model template setup

--- app.py
+++ app.py
@@ -3,8 +3,15 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect, url_for
+from database import db
 
 app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+#db.init_app(app)
+
+#with app.app_context():
+    #db.create_all()
 
 @app.route('/')
 @app.route('/index')
