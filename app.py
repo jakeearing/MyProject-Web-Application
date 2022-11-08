6f5df201838f92d9181650982255ff0d1d09fd3d
Jake Earing <jakeearing7@gmail.com>
2022-11-08 13:59:09 -0500

    Updated html templates, added models, updated app.py with template routes
    HTML files now have a template for our sprint 5 tasks

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ from flask import render_template
 from flask import request
 from flask import redirect, url_for
 from database import db
+from models import Project as Project
+from models import Tasks as Tasks
+from models import User as User
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
 #db.init_app(app)
 
 #with app.app_context():
@@ -22,6 +26,24 @@ def index():
 def projects():
     return render_template('projects.html')
 
-@app.route('/account')
-def account():
-    return render_template('account.html')
+@app.route('/project/<project_id>')
+def project():
+    return render_template('project.html')
+
+@app.route('/projects/new', methods=['GET', 'POST'])
+def new_note():
+    print('request method is',request.method)
+    return render_template('new.html')
+
+@app.route('/notes/edit/<project_id>', methods=['GET', 'POST'])
+def update_project(project_id):
+    
+        return render_template('new.html')
+
+@app.route('/notes/delete/<project_id>', methods=['POST'])
+def delete_project(project_id):
+    
+    return redirect(url_for('get_projects'))
+
+
+app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
--- models.py
+++ models.py
@@ -1,1 +1,36 @@
 from database import db
+
+class Project(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    title = db.Column("title", db.String(200))
+    text = db.Column("text", db.String(100))
+    date = db.Column("date", db.String(50))
+    managerID = db.Column("managerID", db.Integer)
+    
+    def __init__(self, title, text, date, managerID):
+        self.title = title
+        self.text = text
+        self.date = date
+        self.managerID = managerID
+        
+class Tasks(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    title = db.Column("title", db.String(200))
+    text = db.Column("text", db.String(100))
+    
+    def __init__(self, title, text, date, managerID):
+        self.title = title
+        self.text = text
+        self.date = date
+        self.managerID = managerID
+        
+class User(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column("name", db.String(100))
+    email = db.Column("email", db.String(100))
+    password = db.Column("password", db.String(100))
+    
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
