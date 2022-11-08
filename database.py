3a39e96ebc5f5cf4840b8c0727b9092a5aed22ac
WadeStern <wademstern@gmail.com>
2022-11-08 16:08:56 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,3 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+from database import db
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Team23_App.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+#  Bind SQLAlchemy db object to this Flask app
+db.init_app(app)
+
+# Setup models
+with app.app_context():
+    db.create_all()   # run under the app context
