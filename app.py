2d1dca89bfea22c559f18771c1fce608bad0347d
WadeStern <wademstern@gmail.com>
2022-11-08 22:41:51 -0500

    Adding app.py
    app.py is in the instance folder

--- app.py
+++ app.py
@@ -6,21 +6,24 @@ from flask import redirect, url_for
 from database import db
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
-#db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
-#with app.app_context():
-    #db.create_all()
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
+
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
+
 
 @app.route('/account')
 def account():
