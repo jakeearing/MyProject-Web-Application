7997c2f55665510613ed26bfdd97982e12e4d842
Jake Earing <jakeearing7@gmail.com>
2022-11-29 13:41:26 -0500

    App.py all routes added
    All account routes added, as well as project comment route, imports were cleaned up/consolidated

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import os
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from database import db
-from models import Project as Project
-#from models import Task as Task
-from models import User as User
+from models import Project as Project, User as User, Comment as Comment
+from forms import RegisterForm, LoginForm, CommentForm
+import bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'MPTEAM23'
 
 db.init_app(app)
 
@@ -20,14 +18,17 @@ with app.app_context():
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user =  db.session.query(User).filter_by(email='jearing@uncc.edu').one()
-    return render_template('index.html', user=a_user)
+    if session.get('user'):
+        return render_template('index.html', user = session['user'])
+    return render_template('index.html')
 
 @app.route('/projects')
 def get_projects():
-    a_user =  db.session.query(User).filter_by(email='jearing@uncc.edu').one()
-    my_projects = db.session.query(Project).all()
-    return render_template('project/projects.html', projects = my_projects, user = a_user)
+    if session.get('user'):
+        my_projects = db.session.query(Project).all()
+        return render_template('projects.html', projects = my_projects, user=session['user'])
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/projects/<project_id>')
 def get_project(project_id):
@@ -77,6 +78,23 @@ def update_project(project_id):
         my_project = db.session.query(Project).filter_by(id=project_id).one()
     
         return render_template('project/new.html', project=my_project, user=a_user)
+    
+@app.route('/projects/<project_id>/comment', methods=['POST'])
+def new_comment(project_id):
+    if session.get('user'):
+        comment_form = CommentForm()
+        # validate_on_submit only validates using POST
+        if comment_form.validate_on_submit():
+            # get comment data
+            comment_text = request.form['comment']
+            new_record = Comment(comment_text, int(project_id), session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
+
+        return redirect(url_for('get_project', project_id=project_id))
+
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/projects/delete/<project_id>', methods=['POST'])
 def delete_project(project_id):
@@ -86,6 +104,62 @@ def delete_project(project_id):
     db.session.commit()
     
     return redirect(url_for('get_projects'))
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+
+        h_password = bcrypt.hashpw(
+            request.form['password'].encode('utf-8'), bcrypt.gensalt())
+
+        first_name = request.form['firstname']
+        last_name = request.form['lastname']
+
+        new_user = User(first_name, last_name, request.form['email'], h_password)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        session['user'] = first_name
+        session['user_id'] = new_user.id  # access id value from user model of this newly added user
+
+        return redirect(url_for('get_projects'))
+
+    # something went wrong - display register view
+    return render_template('account/register.html', form=form)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    login_form = LoginForm()
+
+    if login_form.validate_on_submit():
+
+        the_user = db.session.query(User).filter_by(email=request.form['email']).one()
+
+        if bcrypt.checkpw(request.form['password'].encode('utf-8'), the_user.password):
+
+            session['user'] = the_user.fname
+            session['user_id'] = the_user.id
+            # render view
+            return redirect(url_for('get_projects'))
+
+        # password check failed
+        # set error message to alert user
+        login_form.password.errors = ["Incorrect username or password."]
+        return render_template("account/login.html", form=login_form)
+    else:
+        # form did not validate or GET request
+        return render_template("account/login.html", form=login_form)
+    
+@app.route('/logout')
+def logout():
+    # check if a user is saved in session
+    if session.get('user'):
+        session.clear()
+
+    return redirect(url_for('index'))
     
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
